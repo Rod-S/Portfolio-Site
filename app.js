cd0c13c7990adeaf37d498ddc66bc21be5dd4543
Rod-S <33836692+Rod-S@users.noreply.github.com>
2018-08-25 00:30:53 -0500

    -updated landing page images -added /project route (unfinished)

--- app.js
+++ app.js
@@ -18,7 +18,27 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
   res.render('about');
-})
+});
+
+app.get('/project/:id', (req, res) => {
+  res.locals = data.projects[0];
+  res.render('project');
+});
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(( err, req, res, next ) => {
+  res.locals.error = err;
+  if (err.status >= 100 && err.status < 600)
+    res.status(err.status);
+  else
+    res.status(500);
+  res.render('error');
+});
 
 
 app.listen(3000, () => {

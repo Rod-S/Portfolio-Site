e361017b63ce65f3bd56ead698d0e6776fec25d6
Rod-S <33836692+Rod-S@users.noreply.github.com>
2018-08-31 14:59:47 -0500

    -Completed draft, Not yet tested for Q/A, cross-browser consistency

--- app.js
+++ app.js
@@ -1,41 +1,44 @@
+//require necessary dependencies
 const express = require('express');
-const path = require('path');
 const app = express();
 const data = require('./data.json');
-const projects = data;
-const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+//setup static route to serve static files in public folder
 app.use('/static', express.static('public'));
 
+//set view engine to read pug files in view folder
 app.set('view engine', 'pug');
 
+//route to home webpage, rendering index.pug
 app.get('/', (req, res) => {
-  res.locals = data;
-  res.render('index', res.locals);
-
+  const { projects } =  data;
+  res.locals = projects;
+  res.render('index', {projects});
 });
 
+//route to about page, rendering about.pug
 app.get('/about', (req, res) => {
   res.render('about');
 });
 
+//route to individual project pages from index route; render project.pug
 app.get('/project/:id', (req, res) => {
   const {id} = req.params;
   res.locals = data.projects[id];
-  console.log(res.locals);
-  console.log(id);
   res.render('project');
 });
 
+//setup error middleware, passing in new error object
 app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
+//render status error page rendering error.pug
 app.use(( err, req, res, next ) => {
   res.locals.error = err;
+  //display specific error status' depending on code
   if (err.status >= 100 && err.status < 600)
     res.status(err.status);
   else
@@ -43,7 +46,7 @@ app.use(( err, req, res, next ) => {
   res.render('error');
 });
 
-
+//start server on localhost port 3000
 app.listen(3000, () => {
   console.log('The application is running on localhost:3000!');
 });
